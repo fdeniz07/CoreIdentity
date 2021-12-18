6dcc85cde52f3767a67d55a6c5cb415de082f95b
Fatih Deniz <deniz.fatih@hotmail.de>
2021-12-19 00:48:47 +0100

    SignUp

--- CoreIdentity/Controllers/HomeController.cs
+++ CoreIdentity/Controllers/HomeController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreIdentity.Models;
+using CoreIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CoreIdentity.Controllers
 {
     public class HomeController : Controller
     {
+        public UserManager<AppUser>  _userManager { get;}
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
         {
             return View();
         }
@@ -16,9 +32,30 @@ namespace CoreIdentity.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignUp()
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+                user.UserName = userViewModel.UserName;
+                user.Email = userViewModel.Email;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+
+                IdentityResult result = await _userManager.CreateAsync(user, userViewModel.Password);
+
+                if (result.Succeeded) // Kayit olduktan sonra kullaniciyi login ekranina gönderip, bilgilerinin tekrar girilmesi en güvenli yoldur
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description); //"" yazmamizin nedeni hatalari frontend tarafindaki asp-validation-summary kismina yani sayfanin en üstünde hatalari getirecektir. Biz "" yerine icerisine ilgili item'i yazabiliriz.
+                    }
+                }
+            }
+            return View(userViewModel); //Hatalari ekle tekrar kullanicinin girdigi bilgileri gönder
         }
     }
 }
--- CoreIdentity/ViewModels/UserViewModel.cs
+++ CoreIdentity/ViewModels/UserViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace CoreIdentity.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreIdentity.ViewModels
 {
     public class UserViewModel
     {
+        [Required(ErrorMessage ="Kullanici ismi gereklidir.")]
+        [Display(Name ="Kullanici Adi")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Telefon No")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email adresi gereklidir.")]
+        [Display(Name = "Email Adresimiz")]
+        [EmailAddress(ErrorMessage ="Email adresiniz dogru formatta degil")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Sifreniz gereklidir.")]
+        [Display(Name = "Sifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
